0c744d0c37b02d03eb5af36e9d0bb677dc61f5ff
Rohit Goud <rohitgoud@sunitavarmas-MacBook-Air.local>
2018-09-20 05:51:41 +0530

    Created LoginVC UI

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Rohit Goud on 20/09/18.
+//  Copyright © 2018 Rohit Goud. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Rohit Goud on 20/09/18.
+//  Copyright © 2018 Rohit Goud. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
